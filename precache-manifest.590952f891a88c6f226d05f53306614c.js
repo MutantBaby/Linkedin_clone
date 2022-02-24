e25a91384c7123f20820aa3ddd1e2c02623a15a8
MutantBaby <shaheryarbakir998877@gmail.com>
2022-02-24 21:02:45 +0500

    Updates

---
+++ precache-manifest.590952f891a88c6f226d05f53306614c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75d73779c6c1ad362061443dd6d059d",
+    "url": "/Linkedin_clone/index.html"
+  },
+  {
+    "revision": "217e60bef5c8b3a457cb",
+    "url": "/Linkedin_clone/static/css/main.b73dd418.chunk.css"
+  },
+  {
+    "revision": "aa5b8ec08e454f048a9a",
+    "url": "/Linkedin_clone/static/js/2.8f5f939b.chunk.js"
+  },
+  {
+    "revision": "b1393abd7ee5c9e2c0619a95401dd6a7",
+    "url": "/Linkedin_clone/static/js/2.8f5f939b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217e60bef5c8b3a457cb",
+    "url": "/Linkedin_clone/static/js/main.69f9352b.chunk.js"
+  },
+  {
+    "revision": "651f8ea1a285156ae946",
+    "url": "/Linkedin_clone/static/js/runtime-main.e6ac5825.js"
+  },
+  {
+    "revision": "c24cde2827448f08d40afd6909011e16",
+    "url": "/Linkedin_clone/static/media/LI-Logo.c24cde28.png"
+  },
+  {
+    "revision": "d492efc706db983e74258dbd348f2208",
+    "url": "/Linkedin_clone/static/media/linkedin.d492efc7.png"
+  }
+]);
